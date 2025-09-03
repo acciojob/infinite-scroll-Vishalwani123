5ea7f77fcb626e063202ceb887eeebaca7ff9123
Vishal Wani <93905384+Vishalwani123@users.noreply.github.com>
2025-09-03 18:35:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
+const infiList = document.getElementById('infi-list');
+let count = 1;
 
+for (let i = 0; i < 10; i++) {
+  const li = document.createElement('li');
+  li.textContent = `Item ${count}`;
+  infiList.appendChild(li);
+  count++;
+}
+
+infiList.addEventListener('scroll', () => {
+  if (infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight) {
+    for (let i = 0; i < 10; i++) {
+      const li = document.createElement('li');
+      li.textContent = `Item ${count}`;
+      infiList.appendChild(li);
+      count++;
+    }
+  }
+});
